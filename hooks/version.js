eae18b7ad1ef651a6487a7b50ee6dd27801c5709
Kaminum42 <kaminum42@qq.com>
2024-04-15 07:31:29 +0800

    fix timezone

--- hooks/version.js
+++ hooks/version.js
@@ -7,5 +7,5 @@ const version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
 // write dist/version.json
 fs.writeFileSync(path.join(__dirname, '../dist/version.json'), JSON.stringify({
     version: version,
-    build_time: new Date()
+    build_time: new Date().toLocaleString()
 }, null, 4));
